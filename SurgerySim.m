d74a996ecfff5bfe9ead6349ee798bcd11a116cc
U-HNLAB-PRE5820\kieran <kieranwarmstrong@gmail.com>
2018-09-04 11:59:30 -0600

    Removed functions that are not being used anymore. Remaned two files for clarity

--- SurgerySim.m
+++ SurgerySim.m
@@ -1,23 +1,14 @@
-% 1. Inverse simulation:
-%   All muscles   
-%   Target motion (preop)
-%    
-% 2. Forward simulation (to represent motor response)
-%   Get incisor plots (this is the preop comparison path)
-%
-% 3. Cases for muscle removal:
-%   Case 1: Deactivation of muscles attached to one-sided process
-%   Case 2: Deactivation of one sided pterygoids muscles
-%   Case 3: Deactivation of submental muscles
-%   Case 4: 
-%
-% 4. Pertubation study with removed muscles inverse.
+%-------------------------------SUMMARY------------------------------------
+% This script generates inverse and forward excitation plots for each group
+% of muscles pre and post op. The excitations signals are collected for the
+% target trajectory of the the jaw in an inverse simulation, those signals
+% are then smoothed and plotted. The smooth signals are used to drive the 
+% forward simulations.
 
-%--------------------CLEAR WORKSPACE AND SHUFFLE RNDGEN--------------------
-clc;
-clear;
-close all;
-rng('shuffle');
+% Refer to the muscle.txt which has all of the muscle definitions. To select
+% muscle to remove from the chewing simulation take note of the number code and
+% select them from the muscles struct. An example of three deferent cases are
+% are shown below in the MUSCLES TO DEACTIVATE section below.
 
 %-------------------------SCRIPT DEFINITIONS------------------------------  
 simDur  = 0.5;
@@ -26,6 +17,7 @@ t = [0:dt:simDur];
 
 %-------------------------MUSCLE DEFINITIONS------------------------------  
 muscles = createmusclestruct('musclekey.txt'); 
+ 
 % Muscle Groups
 temporals  = muscles([1:6]);
 masseters  = muscles([7:8]);
@@ -34,27 +26,31 @@ digastrics = muscles([17:18]);
 mylohyoid  = muscles([19:22]);
 geniohyoid = muscles([23:24]);
 
-% Muscle Groups to be deactivated
-leftsidecorprocess = muscles([3 5]);
-rightsidecorprocess = muscles([4 6]);
-bothcorprocess = muscles([3 4 5 6]);
-leftpterygoids = muscles([11 13 15]);
-righttpterygoids = muscles([12 14 16]);
-allpterygoids = muscles([11 12 13 14 15 16]);
-leftsubmentalmuscles = muscles([17 19 21 23]);
-rightsubmentalmuscles = muscles([18 20 22 24]);
-submentalmuscles = muscles([17 18 19 20 21 22 23 24]);
+% Masseter, Medial Pterygoid, and Temporalis
+leftJawOpeners = muscles([1 3 5 7 9 11]);
+rightJawOpeners = muscles([2 4 6 8 10 12]);
 
-musclesToDeactivate = leftpterygoids;
-muscleDeactivatedDescription = ' Left Pterygoids Removed';
+% Digastricus, Geniohyoid, Lateral pterygoid, Mylohyoid
+leftJawClosers = muscles([13 15 17 19 21 23]);
+rightJawClosers = muscles([14 16 18 20 22 24]);
 
+%------------------------MUSCLES TO DEACTIVATE-----------------------------
+lefttemporals = muscles([1 3 5]);
+leftmedialpterygoid = muscles([11]);
+leftlatalpterygoid = muscles([13 15]);
+
+%***********CHANGE THE NEXT TWO LINES TO SIMULATE SURGERY CASE*************
+musclesToDeactivate = leftmedialpterygoid; 
+plotTitle = 'Left Medial Pterygoid Removed';
+
+%-------------------------ARTISYNTH MODEL NAMES---------------------------
 invModelName = ...
     'artisynth.models.kieran.tmjsurgery.TmjInverseOpenCloseSimulation';
 forwardModelName = ...
     'artisynth.models.kieran.tmjsurgery.ForwardChewing';
 
-outputfilename = strcat('Trajectory Plots');
-mkdir(outputfilename);
+outputFileName = 'Excitation Plots';
+mkdir(outputFileName);
 
 %-------------------------------PREOP------------------------------
 [preopInvExcitations,preopInvICP,preopInvICV] = inversesim(simDur,invModelName);
@@ -77,91 +73,135 @@ compensatedSmoothExcit = smoothexcitationsignal(compensatedExcit(:,2:25));
 [compensatedICPForw,compensatedICVForw,compensatedExcitForw] = ...
     forwardsim(simDur,forwardModelName,compensatedSmoothExcit,muscles,musclesToDeactivate);
 
-%-----------------------------INCISOR PLOTS-----------------------------
-%parameters for figure and panel size
-plotheight=20;
-plotwidth=16;
-subplotsx=2;
-subplotsy=3;   
-leftedge=1.2;
-rightedge=0.4;   
-topedge=1;
-bottomedge=1.5;
-spacex=0.2;
-spacey=0.2;
-fontsize=5;    
-sub_pos=subplotpos(plotwidth,plotheight,leftedge,rightedge,bottomedge,topedge,subplotsx,subplotsy,spacex,spacey);
+%---------------------------EXCITATION PLOTS--------------------------------
+for iplot= 1:1
+    %parameters for figure and panel size
+    plotheight=24.1;
+    plotwidth=17.7;
+    subplotsx=3;
+    subplotsy=4;   
+    leftedge=1.5;
+    rightedge=1.5;   
+    topedge=1.65;
+    bottomedge=1.65;
+    spacex=0.3;
+    spacey=0.5;
+    fontsize=2;    
+    sub_pos=subplotpos(plotwidth,plotheight,leftedge,rightedge,bottomedge,topedge,subplotsx,subplotsy,spacex,spacey);
+    
+    %setting the Matlab figure
+    f=figure('visible','on')
+    clf(f);
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperSize', [plotwidth plotheight]);
+    set(gcf, 'PaperPositionMode', 'manual');
+    set(gcf, 'PaperPosition', [0 0 plotwidth plotheight]);
+    
+    %loop to create axes
+    for i=1:subplotsx
+        for ii=1:subplotsy
+            ax=axes('position',sub_pos{i,ii},'XGrid','off','XMinorGrid','off','FontSize',6,'Box','on','Layer','top');
+            if(iplot == 1)
+                % Column 1 - Jaw Depressors
+                if (i == 3 && ii == 4)
+                    plotexcitations(t,preopExcit,leftJawClosers,postopICPForw, outputFileName,'Preop Excitations');
+                    set(ax,'xticklabel',[])
+                    set(ax,'yticklabel',[])
+                end
+                if(i == 3 && ii == 3)
+                    plotexcitations(t,postopExcit,leftJawClosers,postopICPForw, outputFileName,'Postop Excitations');
+                    set(ax,'xticklabel',[])
+                    set(ax,'yticklabel',[])
+                end
+                if(i == 3 && ii == 2)
+                    plotexcitations(t,compensatedExcitForw,leftJawClosers,compensatedICPForw, outputFileName,'Ipsilateral Compensated');
+                    set(ax,'xticklabel',[])
+                    set(ax,'yticklabel',[])
+                end
+                if(i == 3 && ii == 1)
+                    plotexcitations(t,compensatedExcitForw,rightJawClosers,compensatedICPForw, outputFileName,'Contralateral Compensated');
+                    set(ax,'yticklabel',[])
+                    xlabel(['Time [s]'])
+                end
+                
+                % Column 2 - Jaw Elevators
+                if(i == 2 && ii == 4)
+                    plotexcitations(t,preopExcit,leftJawOpeners,goalICP,outputFileName,'Preop Excitations');
+                    set(ax,'yticklabel',[])
+                    set(ax,'xticklabel',[])
 
-%setting the Matlab figure
-f=figure('visible','on')
-clf(f);
-set(gcf, 'PaperUnits', 'centimeters');
-set(gcf, 'PaperSize', [plotwidth plotheight]);
-set(gcf, 'PaperPositionMode', 'manual');
-set(gcf, 'PaperPosition', [0 0 plotwidth plotheight]);
+                end
+                if(i == 2 && ii == 3)
+                    plotexcitations(t,postopExcit,leftJawOpeners,postopICPForw,outputFileName,'Postop Excitations');
+                    set(ax,'yticklabel',[])
+                    set(ax,'xticklabel',[])
 
-%loop to create axes
-for i=1:subplotsx
-    for ii=1:subplotsy
+                end
+                if(i == 2 && ii == 2)
+                    plotexcitations(t,compensatedExcitForw,leftJawOpeners,compensatedICPForw, outputFileName,'Ipsilateral Compensated');
+                    set(ax,'yticklabel',[])
+                    set(ax,'xticklabel',[])
 
-    ax=axes('position',sub_pos{i,ii},'XGrid','off','XMinorGrid','off','FontSize',fontsize,'Box','on','Layer','top');
-    % Pre and post op cases
-    if(i == 1 && ii == 3)
-        plotincisorposition(goalICP,postopICPForw,[0,0],['Surgery',muscleDeactivatedDescription])
-    end
-    if(i == 2 && ii == 3)
-        plotincisorposition(goalICP,postopICPForw,[90,0],['Surgery',muscleDeactivatedDescription])
-    end
+                end
+                if(i == 2 && ii == 1)
+                    plotexcitations(t,compensatedExcitForw,rightJawOpeners,compensatedICPForw, outputFileName,'Contralateral Compensated');
+                    set(ax,'yticklabel',[])
+                    xlabel(['Time [s]'])
+                end
+                
+                % Column 3 - Trajectory Plots
+                if(i == 1 && ii == 4)
+                    plotincisorposition(goalICP,postopICPForw,compensatedICPForw,[0,0],['Fontal View']);
+                    set(ax,'yticklabel',[])
+                end
+                if(i == 1 && ii == 3)
+                    plotincisorposition(goalICP,postopICPForw,compensatedICPForw,[90,0],['Sagittal View']);
+                    set(ax,'xticklabel',[])
 
-    % Inverse Compensation Results compared to forward sim
-    if(i == 1 && ii == 2)
-        plotincisorposition(compensatedExcitICP,compensatedExcitICP,[0,0],'Inverse Comp')
-    end
-    if(i == 2 && ii == 2)
-        plotincisorposition(compensatedExcitICP,compensatedExcitICP,[90,0],'Inverse Comp')
-    end
+                end
+                if(i == 1 && ii == 2)
+                    plotincisorposition(goalICP,postopICPForw,compensatedICPForw,[0,90],['Superior View']);
+                    set(ax,'zticklabel',[])
 
-    % Goal trajectory forward vs the compensated model forwards
-    if(i == 1 && ii == 1)
-        plotincisorposition(goalICP,compensatedICPForw,[0,0],'Goal Comp')
-        xlabel(['X Axis [mm]']);
+                end
+            end
+            
+            if ii==subplotsy
+                if (i == 1)
+                    title('Lower Mid Incisor Position');
+                elseif (i == 2)
+                    title('Jaw Elevators');
+                elseif (i == 3)
+                    title('Jaw Depressors');
+                end
+            end
+        
+            if i==3
+                yyaxis right
+                ylabel('Excitation [%]');
+                set(gca,'ycolor','k') 
+                
+            end
+            if i==1
+                xlabel('[mm]')
+                 zlabel('[mm]');
+                ylabel('[mm]');
+            end
+        end
     end
-    if(i == 2 && ii == 1)
-        plotincisorposition(goalICP,compensatedICPForw,[90,0],'Goal Comp')
-        ylabel(['Y Axis [mm]']);
-    end
-    if ii==subplotsy
-        if (i == 1)
-            title('Frontal View');
-        elseif (i == 2)
-            title('Transverse View');
-    end     
+    annotation('textbox', [0 0.9 1 0.1], ...
+    'String', plotTitle, ...
+    'EdgeColor', 'none', ...
+    'HorizontalAlignment', 'center')
 end
-
-    if ii>1
-    set(ax,'xticklabel',[])
-    end
-
-    if i>1
-    set(ax,'yticklabel',[])
-    end
-
-    if i==1
-    zlabel('Z Axis [mm]')
-    end
-
-    if ii==1
-   
-    end
-
-    end
-end
-
 %Saving eps with matlab and then producing pdf and png with system commands
 %If using windows or mac you need to download the MikTex commandline tool.
 %https://miktex.org/download
 
-% fileName=['ICPcases'];
+fileName=['Excitations'];
+saveas(gcf,[outputFileName,'/',fileName,'.pdf']);
+
 % print(gcf, '-depsc2','-loose',[outputFileName,'/',fileName,'.eps']);
 % system(['epstopdf ',outputFileName,'/',fileName,'.eps'])
 % system(['convert -density 300 ',outputFileName,'/',fileName,'.eps',outputFileName,'/',fileName,'.png'])
+
