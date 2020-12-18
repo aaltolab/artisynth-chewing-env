d7724de13484a657e59b0ccc9972e054c7458008
jayde <jayde@192.168.1.87>
2020-12-18 07:20:54 -0700

    Added a transformation function from upper jaw movement to lower

--- XML_Processing/Draw_New_Traj.m
+++ XML_Processing/Draw_New_Traj.m
@@ -1,3 +1,14 @@
+% Extract data from the file
+% File_Path = 'D:\Fall_2020_Work_Term\artisynth\China_Data\19971003.xml';
+% %
+% [traj,np,dt] = XML2TRAJ(File_Path,10);
+
+% plot3(traj(1,:,1), traj(1,:,2), traj(1,:,3),traj(2,:,1), traj(2,:,2),
+% traj(2,:,3),traj(3,:,1), traj(3,:,2), traj(3,:,3))
+
+traj_drawn = traj;
+
+% An infinite loop, to exit, use ctrl+C to break.
 while true
     
     disp('Press any key to continue.')
@@ -20,9 +31,9 @@ while true
     for i = 1:np
         
         %     Add points to the animated lines.
-        addpoints(curve1,di(1,i,1), di(1,i,2), di(1,i,3))
-        addpoints(curve2,di(2,i,1), di(2,i,2), di(2,i,3))
-        addpoints(curve3,di(3,i,1), di(3,i,2), di(3,i,3))
+        addpoints(curve1,traj_drawn(1,i,1), traj_drawn(1,i,2), traj_drawn(1,i,3))
+        addpoints(curve2,traj_drawn(2,i,1), traj_drawn(2,i,2), traj_drawn(2,i,3))
+        addpoints(curve3,traj_drawn(3,i,1), traj_drawn(3,i,2), traj_drawn(3,i,3))
         
         %    The interval between each point is set below, by setting it to
         %    dt, it represents the real interval at which the data was
--- XML_Processing/Draw_Traj.m
+++ XML_Processing/Draw_Traj.m
@@ -1,10 +1,12 @@
 % Extract data from the file
-File_Path = 'D:\Fall_2020_Work_Term\artisynth\China_Data\19971003.xml';
-%
-[traj,np,dt] = XML2TRAJ(File_Path,10);
+% File_Path = 'D:\Fall_2020_Work_Term\artisynth\China_Data\19971003.xml';
+% %
+% [traj,np,dt] = XML2TRAJ(File_Path,10);
 
 % plot3(traj(1,:,1), traj(1,:,2), traj(1,:,3),traj(2,:,1), traj(2,:,2),
 % traj(2,:,3),traj(3,:,1), traj(3,:,2), traj(3,:,3))
+
+traj_drawn = traj_o;
 
 % An infinite loop, to exit, use ctrl+C to break.
 while true
@@ -22,16 +24,16 @@ while true
     curve2 = animatedline('LineWidth',2,'Color','r');
     curve3 = animatedline('LineWidth',2,'Color','b');
     
-%     axis([-40 40 -40 40 -40 40])
+    axis([-40 40 -40 40 -40 40])
     
     grid on
     
     for i = 1:np
         
         %     Add points to the animated lines.
-        addpoints(curve1,traj(1,i,1), traj(1,i,2), traj(1,i,3))
-        addpoints(curve2,traj(2,i,1), traj(2,i,2), traj(2,i,3))
-        addpoints(curve3,traj(3,i,1), traj(3,i,2), traj(3,i,3))
+        addpoints(curve1,traj_drawn(1,i,1), traj_drawn(1,i,2), traj_drawn(1,i,3))
+        addpoints(curve2,traj_drawn(2,i,1), traj_drawn(2,i,2), traj_drawn(2,i,3))
+        addpoints(curve3,traj_drawn(3,i,1), traj_drawn(3,i,2), traj_drawn(3,i,3))
         
         %    The interval between each point is set below, by setting it to
         %    dt, it represents the real interval at which the data was
